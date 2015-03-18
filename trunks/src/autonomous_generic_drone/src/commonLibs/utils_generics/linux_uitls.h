e84246860dc546580055a03637b2ce18186c21bf
ricardo <aricardorodriguez@hotmail.com>
2015-03-18 14:55:33 -0300

    finalizacion funcion de rotation de archivos logs

--- trunks/src/autonomous_generic_drone/src/commonLibs/utils_generics/linux_uitls.h
+++ trunks/src/autonomous_generic_drone/src/commonLibs/utils_generics/linux_uitls.h
@@ -8,8 +8,10 @@
   */
 #ifndef LINUX_UITLS_H
 #define LINUX_UITLS_H
-#include <sys/stat.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
 #include <time.h>
 #include <ctime>
 #include <dirent.h>
@@ -17,6 +19,7 @@
 #include <chrono>
 #include <iomanip>
 #include <algorithm>
+#include <unistd.h>
 namespace NSUtils
 {
   typedef struct tm LinuxTimeStructure;
@@ -25,13 +28,9 @@ namespace NSUtils
       ESeconds
   };
 
-  template<EComparatorType,typename T>
-  struct Comparator
-  {
 
-  };
   template<typename T>
-  struct Comparator<EComparatorType::ESeconds,T>
+  struct Comparator
   {
     bool operator()(const T & FirstValue,
                     const T & SecondValue
@@ -41,6 +40,16 @@ namespace NSUtils
               std::get<1>(SecondValue));
     }
   };
+  struct BaseComparator
+  {
+    bool operator()(const std::tuple<std::string,uint64_t> & FirstValue,
+                    const std::tuple<std::string,uint64_t> & SecondValue
+                    )
+    {
+      return (std::get<1>(FirstValue)<
+              std::get<1>(SecondValue));
+    }
+  }ComparatorClass;
 
   struct LinuxUtils
   {
@@ -76,27 +85,36 @@ namespace NSUtils
   {
     return (FirstPart +"/" +SecondPart);
   }
-
+  static int removeFile(const std::string & aFileName)
+  {
+    return (unlink(aFileName.c_str()));
+  }
   struct Tm : std::tm {
+
+    constexpr static int INITIAL_YEAR=100;
+    constexpr static int AVOID_JANUARY=1;
+    constexpr static int DEFAULT_mSECONDS=0;
+    constexpr static int NO_DST=-1;
     int tm_usecs; // [0, 999999] micros after the sec
 
     Tm(const int year, const int month, const int mday, const int hour,
-       const int min, const int sec, const int usecs, const int isDST = -1)
+       const int min, const int sec, const int usecs = 0, const int isDST = -1)
         : tm_usecs{usecs} {
-      tm_year = year - 100; //solamente sacar 100
-      tm_mon = month - 1;    // [0, 11] since Jan
-      tm_mday = mday;        // [1, 31]
-      tm_hour = hour;        // [0, 23] since midnight
-      tm_min = min;          // [0, 59] after the hour
-      tm_sec = sec;          // [0, 60] after the min
-                             //         allows for 1 positive leap second
-      tm_isdst = isDST;      // [-1...] -1 for unknown, 0 for not DST,
-                             //         any positive value if DST.
+      tm_year = year - INITIAL_YEAR;                                            //solamente sacar 100
+      tm_mon = month - AVOID_JANUARY ;                                          // [0, 11] since Jan
+      tm_mday = mday;                                                           // [1, 31]
+      tm_hour = hour;                                                           // [0, 23] since midnight
+      tm_min = min;                                                             // [0, 59] after the hour
+      tm_sec = sec;                                                             // [0, 60] after the min
+                                                                                //         allows for 1 positive leap second
+      tm_isdst = isDST;                                                         // [-1...] -1 for unknown, 0 for not DST,
+                                                                            //         any positive value if DST.
     }
 
     template <typename Clock_t = std::chrono::high_resolution_clock,
               typename MicroSecond_t = std::chrono::seconds>
-    auto to_time_point() -> typename Clock_t::time_point {
+   auto to_time_point() -> typename Clock_t::time_point
+    {
       auto time_c = mktime(this);
       return Clock_t::from_time_t(time_c) /*+ MicroSecond_t{tm_usecs}*/;
     }
@@ -111,10 +129,72 @@ namespace NSUtils
                                       (timeFromCreation->tm_mday),
                                       (timeFromCreation->tm_hour),
                                       (timeFromCreation->tm_min),
-                                      (timeFromCreation->tm_sec),
-                                      0);
+                                      (timeFromCreation->tm_sec)
+                                      );
     const std::chrono::system_clock::time_point time_ellapse =aTimePoint.to_time_point();
     TimeInSeconds=std::chrono::duration_cast<std::chrono::seconds>(now - time_ellapse).count();
+  }
+
+  static void rotateFiles(const char * const aDirectoryPath,const unsigned & MaxFileCount)
+  {
+    const std::string  DirectoryName(aDirectoryPath);
+    std::vector<std::string> FileVector;
+    NSUtils::LinuxUtils::getAllFilesFromDirectory(DirectoryName.c_str(),FileVector);
+    if(!FileVector.empty())
+      {
+        NSUtils::LinuxUtils::LinuxTimeStructure * aPtr=nullptr;
+        std::vector<std::tuple<std::string,uint64_t>> deltaTimeVector;
+        uint64_t SecondsFromCreation=0U;
+        for(auto iterator = FileVector.begin();
+            iterator!= FileVector.end();
+            iterator++)
+          {
+            NSUtils::LinuxUtils::getLastTimeModifiedDate(NSUtils::concatenate(DirectoryName,(*iterator)).c_str(),aPtr);
+            NSUtils::getTimeFromCreation<uint64_t>(aPtr,SecondsFromCreation);
+            deltaTimeVector.emplace_back(std::make_tuple(NSUtils::concatenate(DirectoryName,(*iterator)),SecondsFromCreation));
+            aPtr=nullptr;
+          }
+        typedef NSUtils::Comparator<std::tuple<std::string,uint64_t>> SECONDS_COMPARATOR;
+        std::sort(deltaTimeVector.begin(),deltaTimeVector.end(),NSUtils::ComparatorClass);
+        if(deltaTimeVector.size()>MaxFileCount)
+        {
+          for(auto index=MaxFileCount;index<deltaTimeVector.size();index++)
+            {
+              removeFile(std::get<0>(deltaTimeVector[index]));
+            }
+        }
+      }
+
+  }
+  /**
+   * @brief rotateFilesEx Mejora sobre la funcion anterior verifica el path relativo al HOME del usuario
+   * @param aDirectoryPath
+   * @param aMaxFilePath
+   */
+  static void rotateFilesEx(const char * const aDirectoryPath, const unsigned & aMaxFilePath)
+  {
+    const std::string  DirectoryName(aDirectoryPath);
+  }
+  static void getHomeDirectory(std::string & aHomeDirectoryPath)
+  {
+     aHomeDirectoryPath.clear();
+     struct passwd *pw = getpwuid(getuid());
+       aHomeDirectoryPath.assign(getpwuid(getuid())->pw_dir);
+
+  }
+  static void getPathFromString(std::string & aPathFileName, int & aErrorCode)
+  {
+    aErrorCode=-1;
+    if (aPathFileName.at(0)=='~')
+      {
+        std::string HomeDirectory;
+        getHomeDirectory(HomeDirectory);
+        aErrorCode=HomeDirectory.empty()?-2:0;
+        if (aErrorCode==0)
+          {
+            aPathFileName=HomeDirectory+aPathFileName.substr(1);
+          }
+      }
   }
 
 }
--- trunks/src/autonomous_generic_drone/src/commonLibs/utils_generics/main.cpp
+++ trunks/src/autonomous_generic_drone/src/commonLibs/utils_generics/main.cpp
@@ -20,43 +20,34 @@ int main()
 {
   std::cout<<"utils generics"<<std::endl;
 
-  std::vector<std::string> FileVector;
-  const std::string  DirectoryName("/home/ricardo/Desktop/CODIGO/uc/FLIR_MINSEG_Software/trunk/src/mainControlUnit_UC_SBC340/build/unidadcontrol");
+
+  std::string  DirectoryName("~/Desktop/CODIGO/uc/FLIR_MINSEG_Software/trunk/src/mainControlUnit_UC_SBC340/build/unidadcontrol");
+  int aErrorCode=0;
+  NSUtils::getPathFromString(DirectoryName,aErrorCode);
+  std::cout<<"DirectoryName: "<<DirectoryName<<std::endl;
+  std::cout<<"aErrorCode: "<<aErrorCode<<std::endl;
+  /*
   NSUtils::LinuxUtils::getAllFilesFromDirectory(DirectoryName.c_str(),FileVector);
   NSUtils::LinuxUtils::LinuxTimeStructure * aPtr=nullptr;
   std::vector<std::tuple<std::string,uint64_t>> deltaTimeVector;
+  uint64_t SecondsFromCreation=0U;
   for(auto iterator = FileVector.begin();
       iterator!= FileVector.end();
       iterator++)
     {
-
-     // std::cout<<"nombre: "<<NSUtils::concatenate(DirectoryName,(*iterator))<<std::endl;
       NSUtils::LinuxUtils::getLastTimeModifiedDate(NSUtils::concatenate(DirectoryName,(*iterator)).c_str(),aPtr);
-
-      const std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-      auto aTimePoint=NSUtils::Tm((aPtr->tm_year),
-                                        (aPtr->tm_mon),
-                                        (aPtr->tm_mday),
-                                        (aPtr->tm_hour),
-                                        (aPtr->tm_min),
-                                        (aPtr->tm_sec),
-                                        0);
-      const std::chrono::system_clock::time_point time_ellapse =aTimePoint.to_time_point();
-
-      deltaTimeVector.emplace_back(std::make_tuple(NSUtils::concatenate(DirectoryName,(*iterator)),
-                                                   std::chrono::duration_cast<std::chrono::seconds>(now - time_ellapse).count()));
-
-
-
+      NSUtils::getTimeFromCreation<uint64_t>(aPtr,SecondsFromCreation);
+      deltaTimeVector.emplace_back(std::make_tuple(NSUtils::concatenate(DirectoryName,(*iterator)),SecondsFromCreation));
       aPtr=nullptr;
-      //std::cout<<std::chrono::duration_cast<std::chrono::milliseconds>(tp_nomicro).count()<<std::endl;
     }
-  typedef NSUtils::Comparator<NSUtils::EComparatorType::ESeconds,std::tuple<std::string,uint64_t>> SECONDS_COMPARATOR;
-  std::sort(deltaTimeVector.begin(),deltaTimeVector.end(),SECONDS_COMPARATOR);
+  typedef NSUtils::Comparator<std::tuple<std::string,uint64_t>> SECONDS_COMPARATOR;
+
+  std::sort(deltaTimeVector.begin(),deltaTimeVector.end(),NSUtils::ComparatorClass);
 
   for (auto it=deltaTimeVector.begin(); it!=deltaTimeVector.end(); ++it)
       std::cout << ' ' << std::get<0>((*it))<<" --->> "<<std::get<1>((*it))<<std::endl;
 
+*/
 
   return 0;
 }
