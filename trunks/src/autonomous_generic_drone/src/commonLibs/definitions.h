e82cfd6e2004f29ade4487ed61eb4ce526e47dce
Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2015-03-24 23:45:49 -0300

    se agrego cabecera al archivo definition.h

--- trunks/src/autonomous_generic_drone/src/commonLibs/definitions.h
+++ trunks/src/autonomous_generic_drone/src/commonLibs/definitions.h
@@ -1,3 +1,11 @@
+/**
+*@author : Ricardo Rodriguez
+*@email: aricardorodriguez@hotmail.com
+*@date: 12-03-2015
+*@brief: Archivo de clases utiles para la administracion de archivos en linux
+*
+*
+*/
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 namespace NSCommonsLibs
