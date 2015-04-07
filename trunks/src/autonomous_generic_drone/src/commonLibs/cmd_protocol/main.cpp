44aac02555cbff4a176bcf9000d518e2a11a9229
ricardo <aricardorodriguez@hotmail.com>
2015-04-07 18:18:49 -0300

    -ase agrego definicion de clase protocolo

--- trunks/src/autonomous_generic_drone/src/commonLibs/cmd_protocol/main.cpp
+++ trunks/src/autonomous_generic_drone/src/commonLibs/cmd_protocol/main.cpp
@@ -1,7 +1,30 @@
+#include "state_transition_unit.h"
 
-
-
+namespace NSTest
+{
+  struct Options{};
+  struct CurrentState{
+    CurrentState(const Options * const)
+    {}
+  };
+  struct Event{
+    Event(const Options * const)
+    {}
+  };
+  struct NextState{
+    NextState(const Options * const)
+    {}
+  };
+  struct ActionInterface{
+    ActionInterface(const Options * const)
+    {}
+  };
+}
 int main()
 {
+ const NSTest::Options aOptions;
+ //NSTest::CurrentState * const aPtr = new NSTest::CurrentState(&aOptions);
+  NSProtocol::StateTransitionUnit<NSTest::CurrentState,NSTest::Event,NSTest::NextState,NSTest::ActionInterface> aStateTransitionTable(&aOptions);
+
  return 0;
 }
--- trunks/src/autonomous_generic_drone/src/commonLibs/cmd_protocol/state_transition_unit.h
+++ trunks/src/autonomous_generic_drone/src/commonLibs/cmd_protocol/state_transition_unit.h
@@ -1,15 +1,36 @@
 #ifndef CMD_PROTOCOL_H
 #define CMD_PROTOCOL_H
+#include <tuple>
 namespace NSProtocol
 {
+  //base Case
+  template< class ... Others
+           >
+  class StateTransitionUnit{}; //definicion de clase vacia
+
+
   template<class CurrentState,
            class Event,
            class NextState,
-           class ... ActionInterface
-           ,class >
-  class StateTransitionUnit
+           class ActionInterface,
+           class ... Others
+           >
+  class StateTransitionUnit<CurrentState,Event,NextState,ActionInterface,Others...>
   {
-
+  public:
+    template<typename Options >
+    StateTransitionUnit(const Options * const aOptionsPtr):currentState(aOptionsPtr)
+    ,eventState(aOptionsPtr)
+    ,nextState(aOptionsPtr)
+    ,action(aOptionsPtr)
+    ,Row(&currentState,&eventState,&nextState,&action)
+    {}
+      private:
+    CurrentState currentState;
+    Event eventState;
+    NextState nextState;
+    ActionInterface action;
+    const std::tuple<CurrentState * ,Event * const ,NextState * const ,ActionInterface  * const > Row;
   };
 }
 #endif // CMD_PROTOCOL_H
