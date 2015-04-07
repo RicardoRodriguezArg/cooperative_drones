771f0345ad9a557bd61f9a834d098a94fbcff1c4
ricardo <aricardorodriguez@hotmail.com>
2015-04-07 15:10:22 -0300

    -aclases iniciales para el protocolo generico de comandos

---
+++ trunks/src/autonomous_generic_drone/src/commonLibs/cmd_protocol/state_transition_unit.h
@@ -0,0 +1,15 @@
+#ifndef CMD_PROTOCOL_H
+#define CMD_PROTOCOL_H
+namespace NSProtocol
+{
+  template<class CurrentState,
+           class Event,
+           class NextState,
+           class ... ActionInterface
+           ,class >
+  class StateTransitionUnit
+  {
+
+  };
+}
+#endif // CMD_PROTOCOL_H
